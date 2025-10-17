b03e3b7a55bd1dceea5cde28b295ff57e39062d1
jeison fernandez <jeison.fernandez@sinco.com.co>
2025-10-17 12:00:58 -0500

    🟥Se crea evalua cualquier célula viva con menos de dos vecinas vivas muere

---
+++ GameOfLife/GameOfLife/GameOfLifeTest.cs
@@ -0,0 +1,29 @@
+﻿using AwesomeAssertions;
+namespace GameOfLife;
+
+public class GameOfLifeTest
+{
+    //Cualquier célula viva con menos de dos vecinas vivas muere
+    [Fact]
+    public void Si_CelulaVivaConMenosDeDosVecinas_Debe_Morir()
+    {   
+        //Arrange
+        Celula celula = new Celula();
+        
+        //Act 
+        celula.ValidarSiLaCelulaEstaViva(1);
+        
+        //Assert
+        celula.EstaViva.Should().Be(false);
+    }
+}
+
+public class Celula
+{
+    public void ValidarSiLaCelulaEstaViva(int i)
+    {
+        throw new NotImplementedException();
+    }
+
+    public object EstaViva { get; set; }
+}
---
+++ GameOfLife/GameOfLife/TestList.md
@@ -0,0 +1,3 @@
+--https://www.codurance.com/es/katas/game-of-life-de-conway
+
+--Cualquier célula viva con menos de dos vecinas vivas muere
